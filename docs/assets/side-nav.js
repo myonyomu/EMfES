a62434c76b99ca3182c701570f7e38fff7cee36d
myonyom <myonyomu26@gmail.com>
2021-02-18 13:29:23 +0900

    Change toc style

--- docs/assets/side-nav.js
+++ docs/assets/side-nav.js
@@ -1,7 +1,14 @@
-var toggleBtn = document.querySelector('.sidebar-toggle');
-var sidebar = document.querySelector('.sidebar');
+window.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.querySelector('.sidebar-toggle');
+  const sidebar = document.querySelector('.sidebar');
+  const toggleButtonIcons = document.querySelectorAll('.sidebar-toggle svg');
+  
+  toggleBtn.addEventListener('click', function() {
+    toggleBtn.classList.toggle('is-closed');
+    sidebar.classList.toggle('is-closed');
 
-toggleBtn.addEventListener('click', function() {
-  toggleBtn.classList.toggle('is-closed');
-  sidebar.classList.toggle('is-closed');
-})
+    toggleButtonIcons.forEach((svg) => {
+      svg.classList.toggle('hidden');
+    });
+  })
+});
